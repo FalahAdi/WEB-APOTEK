75d818dbf647b23dd9d587d4465cbbb65833d714
FalahAdi <falahadi04@gmail.com>
2023-07-29 23:47:04 +0700

    FIles Upload

---
+++ UASPAW/resources/views/navbar/navbar.blade.php
@@ -0,0 +1,44 @@
+<nav class="navbar navbar-expand-lg navbar-light " style="background-color: #24025c">
+              <div class="col-md-1" style="display:flex; justify-content:left;">
+                <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_bsjQU1.json"  background="transparent"  speed="1"  style="width: 150px; height: 60px;"  loop  autoplay></lottie-player>              
+              </div>
+  <div class="container-fluid">
+    
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0" style= "font-size:30px; ">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" style="color: #f8f7fa" href="/index">Home</a>
+        </li>
+        
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" style="color: #f8f7fa" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Obat
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item"  href="/CatatObat">Catat Obat</a></li>
+            <li><a class="dropdown-item" href="/ListObat">Daftar Obat</a></li>
+            
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" style="color: #f8f7fa" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Pembelian
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="CatatPembelian">Catat Pembelian</a></li>
+            <li><a class="dropdown-item" href="ListPembelian">Daftar Pembelian</a></li>
+            
+          </ul>
+        </li>
+        
+      </ul>
+      <h2  style="color: #f8f7fa">
+         WEB APOTEK WANNA BE
+      </h2> 
+    </div>
+  </div>
+  <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+</nav>
